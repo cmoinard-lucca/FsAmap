628d1e623a27d691b6c8aeff33f369a7ec6d14b2
Christophe Moinard <cmoinard@lucca.fr>
2020-03-03 18:48:54 +0100

    Section 1 : solution

---
+++ FsAmap.Domain/TypesProduit.fs
@@ -0,0 +1,27 @@
+﻿namespace FsAmap.Domain
+
+type Prix = decimal
+type Produit =
+    {
+        id: int
+        nom: string
+    }
+
+type [<Measure>] kg
+
+type Quantité =
+    | ``À l'unité`` of int
+    | ``Au poids`` of decimal<kg>
+
+type ProduitEnVrac = Produit * Prix
+type PanierDuJour =
+    {
+        prix: Prix
+        produits: (Produit * Quantité) list
+    }
+    
+type PanierPersonnalisé = (Produit * Quantité) list
+    
+type Panier =
+    | PanierDuJour of PanierDuJour
+    | PanierPersonnalisé of PanierPersonnalisé 
